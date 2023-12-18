336537678c7f5adcccaa2d9ad6a57cf7d6f90d88
songjaey <songjaey88@gmail.com>
2023-12-18 09:36:49 +0900

    kkk

--- study/src/study121801/main_class.java
+++ study/src/study121801/main_class.java
@@ -3,7 +3,6 @@ package study121801;
 public class main_class {
 	
 		public static void main(String[] args) {
-			System.out.println("Hello world!");	
-			System.out.println("Hello world!");	
+			System.out.println("Hello world!");
 		}
 }
