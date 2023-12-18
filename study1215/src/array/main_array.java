e77e37a728c3a94bb38b2600bef68b273d2e10d1
songjaey <songjaey88@gmail.com>
2023-12-18 10:14:41 +0900

    song_prac

---
+++ study1215/src/array/main_array.java
@@ -0,0 +1,12 @@
+package array;
+
+public class main_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aaa");
+		System.out.println();
+		System.out.println("test");
+	}
+
+}
---
+++ study1215/src/study121801/main_class.java
@@ -0,0 +1,7 @@
+package study121801;
+
+public class main_class {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");	
+	}
+}
